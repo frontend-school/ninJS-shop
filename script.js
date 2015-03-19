f27c75025e2694e60f0e04f486e6f27e7b3d262e
antongusar <Anton_Gusar@EPUAKYIW0714.kyiv.epam.com>
2015-03-19 18:57:57 +0200

    qwe

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-var divHolder = new Object;
+var divHolder = {}; // or var divHolder = new Object;
 divHolder.count = 0;
 divHolder.killedCount = 0;
 var i = 0;
 
+/*
+var mainDivWidth = 1000;
+var mainDivHeight = 800;
+*/
+/*
+var mainDivLeftOffset;
+mainDivLeftOffset = document.getElementById('main').offsetLeft;
+var mainDivHeight;
+mainDivHeight = document.getElementById('main').scrollHeight;
+*/
 
 function createDiv () {
     i = ++divHolder.count; // increase variable showing div count
     document.getElementById('generatedCount').innerHTML = divHolder.count;
     divHolder['div' + i] = document.createElement('div'); // create div
-    var randLeft = Math.floor(Math.random() * 1000); // generate random number from 0 to 1000
-    divHolder['div' + i].style.left = randLeft;   // make left offset random
+
 
     var randWidth = Math.floor(Math.random() * (300-50) + 50); // div random width
     var randHeight = Math.floor(Math.random() * (100-50) + 50); // div random height
@@ -30,7 +39,11 @@ function createDiv () {
                 divHolder['div' + i].style.WebkitAnimation = 'slide 7s both';
                 break;
         }
-    }
+    };
+
+    var randLeftFunc = function () {
+        divHolder['div' + i].style.left = (Math.floor(Math.random() * (1000 - randWidth))) ; // generate random number from 0 to 1000 subtracting random width of generated div
+    };
 
     switch (randClassName) {
         case 1:
@@ -38,26 +51,24 @@ function createDiv () {
             divHolder['div' + i].style.width = randWidth;
             divHolder['div' + i].style.height = randHeight;
             randSpeedFunc();
+            randLeftFunc();
             break;
         case 2:
-            //var randWidthTri = Math.floor(Math.random() * (300-50) + 50); // div random width
             divHolder['div' + i].className = 'generatedDiv triangle';
             divHolder['div' + i].style.borderLeft = randWidth/2+'px solid transparent'; // divide by 2 because value assigns for 2 sides
             divHolder['div' + i].style.borderRight = randWidth/2+'px solid transparent';
             divHolder['div' + i].style.borderBottom = randWidth/2+'px solid forestgreen';
             randSpeedFunc();
+            randLeftFunc();
             break;
         case 3:
             divHolder['div' + i].className = 'generatedDiv circle';
-            var randWidthCircle = Math.floor(Math.random() * (300-50) + 50); // div.circle random width and height
-            divHolder['div' + i].style.width = randWidthCircle;
-            divHolder['div' + i].style.height = randWidthCircle;
+            divHolder['div' + i].style.width = randWidth;
+            divHolder['div' + i].style.height = randWidth;
             randSpeedFunc();
+            randLeftFunc();
             break;
     }
-
-    // Random speed
-
 
     divHolder['div' + i].setAttribute('onmousedown','killDiv(this.id)'); // add event for killing specific div by mousedown
     divHolder['div' + i].setAttribute('id', i); // add id attribute
@@ -76,21 +87,21 @@ function killDiv (idTArget) {
 
 
 function checkTopValue () {
-    for (k=1; k<i; ++k) {
+    for (var k=1; k<i; ++k) {
         divHolder['div' + k].innerHTML = divHolder['div' + k].offsetTop;
         if (divHolder['div' + k].innerHTML == 800 && divHolder['div' + k].innerHTML != 'game over') {
             divHolder['div' + k].innerHTML = 'game over';
             document.getElementById('alert').style.visibility = 'visible';
         }
     }
-        /* не работало как надо. код находил к-тый див через getElementById, я переделал, чтобы сразу брало значение из свойства объекта к-того дива (offsetTop)
-        for (k=1; k<10; k++) {
-        //for (k in divHolder.)
-            newDivTopPos = getComputedStyle(document.getElementById(k)).getPropertyValue('top'); // finds kth div top property and assigns it to variable
-            divHolder['div'+k].innerHTML = newDivTopPos; // assign to object
-            if (divHolder['div'+k].innerHTML == "800px" && divHolder['div'+k].innerHTML != 'game over') { divHolder['div'+k].innerHTML = 'game over' }
-        }
-        */
+    /* не работало как надо. код находил к-тый див через getElementById, я переделал, чтобы сразу брало значение из свойства объекта к-того дива (offsetTop)
+     for (k=1; k<10; k++) {
+     //for (k in divHolder.)
+     newDivTopPos = getComputedStyle(document.getElementById(k)).getPropertyValue('top'); // finds kth div top property and assigns it to variable
+     divHolder['div'+k].innerHTML = newDivTopPos; // assign to object
+     if (divHolder['div'+k].innerHTML == "800px" && divHolder['div'+k].innerHTML != 'game over') { divHolder['div'+k].innerHTML = 'game over' }
+     }
+     */
 }
 
 
