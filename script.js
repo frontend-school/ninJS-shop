4daa4012a9ccd5433ce42eddce1e0955b8745761
antongusar <Anton_Gusar@EPUAKYIW0714.kyiv.epam.com>
2015-03-20 16:19:35 +0200

    q

--- script.js
+++ script.js
@@ -2,35 +2,26 @@ var divHolder = {}; // or var divHolder = new Object;
 divHolder.count = 0;
 divHolder.killedCount = 0;
 var i = 0;
-
-/*
-var mainDivWidth = 1000;
-var mainDivHeight = 800;
-*/
-/*
-var mainDivLeftOffset;
-mainDivLeftOffset = document.getElementById('main').offsetLeft;
-var mainDivHeight;
-mainDivHeight = document.getElementById('main').scrollHeight;
-*/
+var play = true;
 
 function createDiv () {
+    if (play) {
     i = ++divHolder.count; // increase variable showing div count
     document.getElementById('generatedCount').innerHTML = divHolder.count;
     divHolder['div' + i] = document.createElement('div'); // create div
 
 
-    var randWidth = Math.floor(Math.random() * (300-50) + 50); // div random width
-    var randHeight = Math.floor(Math.random() * (100-50) + 50); // div random height
+    var randWidth = Math.round(Math.random() * (300-50) + 50); // div random width
+    var randHeight = Math.round(Math.random() * (100-50) + 50); // div random height
 
 
-    var randClassName = Math.floor(Math.random() * 3 + 1);
+    var randClassName = Math.round(Math.random() * (2-1) + 1);
 
     var randSpeedFunc = function randSpeedFuncBody () {
-        var randSpeed = Math.floor(Math.random() * 3 + 1);
+        var randSpeed = Math.round(Math.random() * (3-1) + 1);
         switch (randSpeed) {
             case 1:
-                divHolder['div' + i].style.WebkitAnimation = 'slide 2s both';
+                divHolder['div' + i].style.WebkitAnimation = 'slide 3s both';
                 break;
             case 2:
                 divHolder['div' + i].style.WebkitAnimation = 'slide 5s both';
@@ -42,7 +33,7 @@ function createDiv () {
     };
 
     var randLeftFunc = function () {
-        divHolder['div' + i].style.left = (Math.floor(Math.random() * (1000 - randWidth))) ; // generate random number from 0 to 1000 subtracting random width of generated div
+        divHolder['div' + i].style.left = (Math.round(Math.random() * (1000 - randWidth))) ; // generate random number from 0 to 1000 subtracting random width of generated div
     };
 
     switch (randClassName) {
@@ -54,17 +45,17 @@ function createDiv () {
             randLeftFunc();
             break;
         case 2:
-            divHolder['div' + i].className = 'generatedDiv triangle';
-            divHolder['div' + i].style.borderLeft = randWidth/2+'px solid transparent'; // divide by 2 because value assigns for 2 sides
-            divHolder['div' + i].style.borderRight = randWidth/2+'px solid transparent';
-            divHolder['div' + i].style.borderBottom = randWidth/2+'px solid forestgreen';
+            divHolder['div' + i].className = 'generatedDiv circle';
+            divHolder['div' + i].style.width = randWidth;
+            divHolder['div' + i].style.height = randWidth;
             randSpeedFunc();
             randLeftFunc();
             break;
         case 3:
-            divHolder['div' + i].className = 'generatedDiv circle';
-            divHolder['div' + i].style.width = randWidth;
-            divHolder['div' + i].style.height = randWidth;
+            divHolder['div' + i].className = 'generatedDiv triangle';
+            divHolder['div' + i].style.borderLeft = randWidth/2+'px solid transparent'; // divide by 2 because value assigns for 2 sides
+            divHolder['div' + i].style.borderRight = randWidth/2+'px solid transparent';
+            divHolder['div' + i].style.borderBottom = randWidth/2+'px solid green';
             randSpeedFunc();
             randLeftFunc();
             break;
@@ -73,8 +64,7 @@ function createDiv () {
     divHolder['div' + i].setAttribute('onmousedown','killDiv(this.id)'); // add event for killing specific div by mousedown
     divHolder['div' + i].setAttribute('id', i); // add id attribute
     document.getElementById('main').appendChild(divHolder['div' + i]); // insert div into #main
-}
-
+} else {return}}
 
 
 function killDiv (idTArget) {
@@ -87,12 +77,28 @@ function killDiv (idTArget) {
 
 
 function checkTopValue () {
-    for (var k=1; k<i; ++k) {
-        divHolder['div' + k].innerHTML = divHolder['div' + k].offsetTop;
-        if (document.getElementById('main').offsetHeight < (divHolder['div' + k].offsetHeight + divHolder['div' + k].offsetTop)) {
-            document.getElementById('alert').style.visibility = 'visible';
+    if (divHolder.count != 0 && play == true) {
+
+        for (var k=1; k <= i; k++) {
+            //divHolder['div' + k].innerHTML = divHolder['div' + k].offsetTop; // input
+            var mainDivHeight = document.getElementById('main').offsetHeight; // assigns main div height
+            var generatedDivOffsetTop = divHolder['div' + k].offsetTop; // assigns generated div distance from top of main div
+            if ( mainDivHeight < (divHolder['div' + k].offsetHeight + generatedDivOffsetTop)) {
+                document.getElementById('alert').style.visibility = 'visible';
+
+                for (var l=1; l <= divHolder.count; ++l) {
+                    try {
+                    document.getElementById(l).remove(); // executing in try allows to avoid stopping program because of error
+                    } catch (err) {
+                        //console.log(err);
+                    }
+                }
+                play = false;
+            }
         }
     }
+
+    if (play) {setTimeout(checkTopValue, 100)}
     /* не работало как надо. код находил к-тый див через getElementById, я переделал, чтобы сразу брало значение из свойства объекта к-того дива (offsetTop)
      for (k=1; k<10; k++) {
      //for (k in divHolder.)
@@ -104,9 +110,8 @@ function checkTopValue () {
 }
 
 
-setInterval(checkTopValue, 100);
 
-
+setTimeout(checkTopValue, 100);
 
 /*
  var a = screen
