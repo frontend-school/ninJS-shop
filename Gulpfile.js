59371cdc61239a2cf0cdf2c5437fe08174641fa5
Anton Gusar <anton.gusar@gmail.com>
2015-04-03 12:39:45 +0300

    img copy task

--- Gulpfile.js
+++ Gulpfile.js
@@ -29,6 +29,11 @@ gulp.task('js', function () {
         .pipe(gulp.dest('dist/js/'));
 });
 
+gulp.task('img', function () {
+    gulp.src('src/img/*')
+        .pipe(gulp.dest('dist/img/'));
+});
+
 gulp.task('bower', function () {
     gulp.src('bower_components/**')
         .pipe(gulp.dest('dist/vendor/'));
@@ -38,8 +43,9 @@ gulp.task('watch', function () {
     gulp.watch('src/index.html', ['html']);
     gulp.watch('src/css/*', ['css']);
     gulp.watch('src/js/*', ['js']);
+    gulp.watch('src/img/*', ['img']);
     gulp.watch('bower_components/**', ['bower']);
-    gulp.watch(['dist/index.html','dist/js/*','dist/css/*']).on('change', function (event) {
+    gulp.watch(['dist/index.html','dist/css/*', 'dist/js/*', 'dist/img/*']).on('change', function (event) {
         console.log(event.path + ' has ' + event.type);
         browserSync.reload();
     });
