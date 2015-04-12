4d9f83c347bb93a3661b03c0a8a584ba69ec10b9
mazzur <mazzur55@gmail.com>
2015-04-13 01:37:53 +0300

    add styl sourcemap

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp'),
     del = require('del'),
     jade = require('gulp-jade'),
     data = require('gulp-data'),
+    sourcemaps = require('gulp-sourcemaps'),
     paths = {
         src: {
             root: './src',
@@ -27,31 +28,14 @@ var gulp = require('gulp'),
     };
 
 gulp.task('default', ['build','serve', 'watch']);
-gulp.task('build', ['bower','jade','styl','img']);
+gulp.task('build', ['bower','jade','styl','img','js']);
 
-gulp.task('serve', ['bower', 'jade', 'styl', 'img'], function() {
-    browserSync(['./build/**/**.**'],{
-        server: paths.dist.root
-    });
-});
-
-gulp.task('bower', function() {
+gulp.task('bower', ['clean'], function() {
     gulp.src(paths.src.bower)
         .pipe(gulp.dest(paths.dist.vendor));
 });
 
-gulp.task('watch', function(){
-    gulp.watch(paths.src.jade, ['jade']);
-    gulp.watch(paths.src.styl, ['styl']);
-    gulp.watch(paths.src.js, ['js']);
-    gulp.watch([paths.src.jade, paths.src.styl, paths.src.js])
-        .on('change', function (file) {
-            console.log(file.path, 'has changed');
-            browserSync.reload();
-        });
-});
-
-gulp.task('jade', function() {
+gulp.task('jade',['clean'], function() {
     gulp.src(paths.src.jade_main)
         .pipe(data(function() {
             return require(paths.src.json);
@@ -60,21 +44,40 @@ gulp.task('jade', function() {
         .pipe(gulp.dest(paths.dist.root));
 });
 
-gulp.task('styl', function() {
+gulp.task('styl',['clean'], function() {
     gulp.src(paths.src.styl_main)
+        .pipe(sourcemaps.init())
         .pipe(stylus())
         .pipe(concat('styles.css'))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(paths.dist.css));
 });
 
-gulp.task('js', function() {
+gulp.task('js',['clean'], function() {
     gulp.src(paths.src.js)
         .pipe(gulp.dest(paths.dist.js));
 });
 
-gulp.task('img', function() {
+gulp.task('img',['clean'], function() {
     gulp.src(paths.src.img)
         .pipe(gulp.dest(paths.dist.img));
+});
+
+gulp.task('serve', ['bower', 'jade', 'styl', 'img'], function() {
+    browserSync(['./build/**/**.**'],{
+        server: paths.dist.root
+    });
+});
+
+gulp.task('watch', ['clean'], function(){
+    gulp.watch(paths.src.jade, ['jade']);
+    gulp.watch(paths.src.styl, ['styl']);
+    gulp.watch(paths.src.js, ['js']);
+    gulp.watch([paths.src.jade, paths.src.styl, paths.src.js])
+        .on('change', function (file) {
+            console.log(file.path, 'has changed');
+            browserSync.reload();
+        });
 });
 
 gulp.task('clean', function (cb) {
