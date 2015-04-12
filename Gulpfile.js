a03322682777dad8c4ec99741b7c60a2198034f9
mazzur <mazzur55@gmail.com>
2015-04-13 02:48:38 +0300

    fixed autoprefix sourcemap, livereload, clean

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
     data = require('gulp-data'),
     sourcemaps = require('gulp-sourcemaps'),
     autoprefixer = require('gulp-autoprefixer'),
+
     paths = {
         src: {
             root: './src',
@@ -21,6 +22,7 @@ var gulp = require('gulp'),
         },
         dist: {
             root: './dist',
+            html: './dist/index.html',
             css: './dist/css',
             js: './dist/js',
             img: './dist/img',
@@ -29,14 +31,14 @@ var gulp = require('gulp'),
     };
 
 gulp.task('default', ['build','serve', 'watch']);
-gulp.task('build', ['bower','jade','styl','autoprefix','img','js']);
+gulp.task('build', ['clean','bower','jade','styl','img','js']);
 
-gulp.task('bower', ['clean'], function() {
+gulp.task('bower', function() {
     gulp.src(paths.src.bower)
         .pipe(gulp.dest(paths.dist.vendor));
 });
 
-gulp.task('jade',['clean'], function() {
+gulp.task('jade', function() {
     gulp.src(paths.src.jade_main)
         .pipe(data(function() {
             return require(paths.src.json);
@@ -45,30 +47,29 @@ gulp.task('jade',['clean'], function() {
         .pipe(gulp.dest(paths.dist.root));
 });
 
-gulp.task('styl',['clean'], function() {
+gulp.task('styl', function() {
     gulp.src(paths.src.styl_main)
         .pipe(sourcemaps.init())
         .pipe(stylus())
         .pipe(concat('styles.css'))
+        .pipe(autoprefixer())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(paths.dist.css));
 });
 
-gulp.task('autoprefix', ['styl'], function () {
-    return gulp.src('./dist/css/styles.css')
-        .pipe(autoprefixer({
-            browsers: ['> 0.5%'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('./dist/css'));
+gulp.task('styl-no-sourcemap', function() {
+    gulp.src(paths.src.styl_main)
+        .pipe(stylus())
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest(paths.dist.css));
 });
 
-gulp.task('js',['clean'], function() {
+gulp.task('js', function() {
     gulp.src(paths.src.js)
         .pipe(gulp.dest(paths.dist.js));
 });
 
-gulp.task('img',['clean'], function() {
+gulp.task('img', function() {
     gulp.src(paths.src.img)
         .pipe(gulp.dest(paths.dist.img));
 });
@@ -79,17 +80,19 @@ gulp.task('serve', ['bower', 'jade', 'styl', 'img'], function() {
     });
 });
 
-gulp.task('watch', ['clean'], function(){
+gulp.task('watch', function(){
     gulp.watch(paths.src.jade, ['jade']);
     gulp.watch(paths.src.styl, ['styl']);
     gulp.watch(paths.src.js, ['js']);
     gulp.watch([paths.src.jade, paths.src.styl, paths.src.js])
         .on('change', function (file) {
             console.log(file.path, 'has changed');
-            browserSync.reload();
+            setTimeout(function(){
+                browserSync.reload();
+            }, 100);
         });
 });
 
 gulp.task('clean', function (cb) {
-    del(paths.dist.root, cb);
+    del.sync(paths.dist.root, cb);
 });
