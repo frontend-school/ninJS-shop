cb9c3ce69b13fac19d19c8d23215d21171058949
mazzur <mazzur55@gmail.com>
2015-04-26 12:02:53 +0300

    [Layout] - basket block

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
-dsf
+window.addEventListener('load', function() {
+
+    document.getElementsByClassName('basket-toggle')[0].addEventListener('click', function () {
+        document.getElementsByClassName('basket')[0].classList.toggle('hide');
+    });
+
+});
